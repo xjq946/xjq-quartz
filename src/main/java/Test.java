179b0e03279681f89137bf27da4c68512bef934b
xiejiqing <xjq111006@163.com>
2020-04-05 16:18:02 +0800

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,21 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.lidahai.job.ClusterQuartz;
+
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        ClusterQuartz clusterQuartz = (ClusterQuartz)context.getBean("clusterQuartz");
+        clusterQuartz.printUserInfo();
+
+        while (true)
+        {
+
+        }
+    }
+}
